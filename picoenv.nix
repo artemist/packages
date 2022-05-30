638468cb84179f650fcf7ff33f4cdb690ad9d728
Artemis Tosini <me@artem.ist>
2022-05-30 19:30:45 +0000

    Fix picoenv

--- picoenv.nix
+++ picoenv.nix
@@ -1,23 +1,12 @@
 with import <nixpkgs> { };
 let
-  openocd_src = fetchFromGitHub {
-    owner = "raspberrypi";
-    repo = "openocd";
-    rev = "14c0d0d330bd6b2cdc0605ee9a9256e5627a905e";
-    sha256 = "1hdfy7nzcgzs4y68hq6gczh02v75kw4az0hgbzxsvs13796j3fx3";
-    fetchSubmodules = true;
-  };
   sdk_src = fetchFromGitHub {
+    fetchSubmodules = true;
     owner = "raspberrypi";
     repo = "pico-sdk";
-    fetchSubmodules = true;
-    rev = "0f3b7951167cf672afdcb34a58ddd0e363ae886f";
-    sha256 = "1mq66v8dndjsv8ddkmphb2bhmgjnsyqb142f1hbqlckqzra2vqml";
+    rev = "1.3.1";
+    sha256 = "sha256-Xf8QSQIe7XCMunljioC5/MhXcuTmyN7J6pPQFM3ico8=";
   };
-  openocd_picoprobe = openocd.overrideAttrs (oldAttrs: rec {
-    src = openocd_src;
-    configureFlags = oldAttrs.configureFlags ++ [ "--enable-picoprobe" ];
-  });
 in
 mkShell rec {
   name = "pico-env";
@@ -31,7 +20,7 @@ mkShell rec {
     python3
     gcc
     libusb
-    openocd_picoprobe
+    openocd
     gcc-arm-embedded
   ];
 
